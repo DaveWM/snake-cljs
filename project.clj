bc383ff40a432a221b2250f497b4ef5f7612d3cb
David Martin <dwmartin41@gmail.com>
2018-08-14 22:27:17 +0100

    Upgraded clojurescript to fix build

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject davewm/snake "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.908"]
+                 [org.clojure/clojurescript "1.10.339"]
                  [play-cljs "0.10.2"]]
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7"]]
@@ -16,7 +16,8 @@
                         :compiler     {:main       "snake.core"
                                        :asset-path "js/out"
                                        :output-to  "resources/public/js/main.js"
-                                       :optimizations :advanced}}]}
+                                       :optimizations :advanced
+                                       :infer-externs true}}]}
   :figwheel {:nrepl-port 9002}
 
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.4-6"]]}})
