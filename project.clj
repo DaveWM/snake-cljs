fd7a768f955d4142d120ad051ef8be4d3382cdeb
David Martin <dwmartin41@gmail.com>
2018-08-14 22:12:43 +0100

    Add prod build

--- project.clj
+++ project.clj
@@ -2,14 +2,21 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.908"]
                  [play-cljs "0.10.2"]]
-  :plugins [[lein-figwheel "0.5.16"]]
+  :plugins [[lein-figwheel "0.5.16"]
+            [lein-cljsbuild "1.1.7"]]
   :cljsbuild {:builds [{:id           "dev"
                         :source-paths ["src/"]
                         :figwheel     true
                         :compiler     {:main       "snake.core"
                                        :asset-path "js/out"
                                        :output-to  "resources/public/js/main.js"
-                                       :output-dir "resources/public/js/out"}}]}
+                                       :output-dir "resources/public/js/out"}}
+                       {:id           "prod"
+                        :source-paths ["src/"]
+                        :compiler     {:main       "snake.core"
+                                       :asset-path "js/out"
+                                       :output-to  "resources/public/js/main.js"
+                                       :optimizations :advanced}}]}
   :figwheel {:nrepl-port 9002}
 
   :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.4-6"]]}})
